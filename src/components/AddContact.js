d8c65bdb355969ee84b1125c2ac808b47bf3ed78
Alberto Jorge Perez Yañez <alberto.perez.1509@outlook.com>
2020-02-13 20:59:50 -0600

    Se agrego setState para realizar el cambio en la propiedad contact de vacio a ContactsData

---
+++ src/components/AddContact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddContact = (props) => {
+  return (
+    <div className="">
+      <button type="button" name="" id="" className="btn btn-primary btn-lg " onClick={props.metodo}>Agregar</button>
+    </div>
+  );
+}
+
+export default AddContact;
--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
 import contactsData from "./../ContactsData";
 import ContactsCard from "./ContactCard";
+import AddContact from "./AddContact";
 
 class Directory extends Component {
   constructor() {
     super();
     this.state = {
-      contacts: contactsData
+      contacts: []
     };
   }
+  addHandler = () => {
+    this.setState({
+      contacts: contactsData
+    });
+  };
   render() {
     const cards = this.state.contacts.map((contact, idx) => (
       <ContactsCard info={contact} key={idx} />
     ));
-    return <div className="card-columns container mt-5">{cards}</div>;
+
+    const Agregar = () => {
+      console.log("Este botón agregara un contacto al directorio");
+    };
+    
+    return (
+      <div>
+        <div className="card-columns container mt-5">
+          {this.state.contacts.length === 0 ?"No hay contactos disponibles" : cards} 
+          <AddContact metodo={this.addHandler} />
+          </div>
+      </div>
+    );
   }
 }
 
